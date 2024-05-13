9579f704df75fc0325c4c494cae7c982dd729f8d
yisraelevine <93338153+yisraelevine@users.noreply.github.com>
2024-05-13 08:22:50 -0400

    Update upload-csv.php

--- admin-page/upload-csv.php
+++ admin-page/upload-csv.php
@@ -1,27 +1,39 @@
-<form method="post" enctype="multipart/form-data">
-    <input type="file" name="csv_file" accept=".csv" />
-    <input type="submit" name="submit" value="Upload CSV" />
-</form>
-
 <?php
-if (isset($_FILES['csv_file']['tmp_name'])) {
+if (isset($_FILES['csv_file']['tmp_name']) && !empty($_FILES['csv_file']['tmp_name'])) {
     $csv_file_path = $_FILES['csv_file']['tmp_name'];
 
-    global $wpdb;
-    $table_name = $wpdb->prefix . 'stories';
-
     if (($handle = fopen($csv_file_path, "r")) !== FALSE) {
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'stories';
+
+        $sql = "INSERT INTO $table_name (name, url, new, phone) VALUES ";
+        $value_placeholders = array();
+
         while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-            $wpdb->insert(
-                $table_name,
-                array(
-                    'name' => $data[0],
-                    'url' => $data[1],
-                    'new' => $data[2] == 1,
-                    'phone' => $data[3] == 1
-                )
-            );
+            if (count($data) != 4) {
+                continue;
+            }
+
+            $value_placeholders[] = $wpdb->prepare("(%s, %s, %b, %b)", $data[0], $data[1], $data[2] == 1, $data[3] == 1);
+
+            if (count($value_placeholders) >= 1000) {
+                $sql .= implode(", ", $value_placeholders);
+                $wpdb->query($sql);
+                $sql = "INSERT INTO $table_name (name, url, new, phone) VALUES ";
+                $value_placeholders = array();
+            }
         }
+
+        if (!empty($value_placeholders)) {
+            $sql .= implode(", ", $value_placeholders);
+            $wpdb->query($sql);
+        }
+
         fclose($handle);
+    } else {
+        echo "Error opening CSV file.";
     }
+} else {
+    echo "Please upload a CSV file.";
 }
+?>

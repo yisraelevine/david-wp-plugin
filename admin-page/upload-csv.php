ba75d68ecc39a5b1599f379cd1306b5f1b67f649
yisraelevine <93338153+yisraelevine@users.noreply.github.com>
2024-05-13 07:47:48 -0400

    Fix

--- admin-page/upload-csv.php
+++ admin-page/upload-csv.php
@@ -7,9 +7,20 @@
 if (isset($_FILES['csv_file']['tmp_name'])) {
     $csv_file_path = $_FILES['csv_file']['tmp_name'];
 
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'stories';
+
     if (($handle = fopen($csv_file_path, "r")) !== FALSE) {
         while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-            echo implode(',', $data);
+            $wpdb->insert(
+                $table_name,
+                array(
+                    'name' => $data[0],
+                    'url' => $data[1],
+                    'new' => $data[2] === 1,
+                    'phone' => $data[3] === 1
+                )
+            );
         }
         fclose($handle);
     }

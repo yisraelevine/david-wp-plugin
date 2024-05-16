64f3d22d73e59f6ba0c1fa9d28c6908b9d3fd37f
yisraelevine <93338153+yisraelevine@users.noreply.github.com>
2024-05-15 20:31:31 -0400

    Update api.php

--- api/api.php
+++ api/api.php
@@ -1,10 +1,14 @@
 <?php
-add_action('rest_api_init', 'register_custom_endpoint');
+add_action('rest_api_init', 'register_list_endpoint');
 
-function register_custom_endpoint() {
+function register_list_endpoint() {
     register_rest_route('stories/v1', '/list/', array(
         'methods' => 'GET',
         'callback' => 'list_endpoint_callback',
+    ));
+   register_rest_route('stories/v1', '/url/', array(
+        'methods' => 'GET',
+        'callback' => 'url_endpoint_callback',
     ));
 }
 
@@ -13,4 +17,19 @@ function list_endpoint_callback($data) {
     $table_name = $wpdb->prefix . 'stories';
     $results = $wpdb->get_results("SELECT id, name, new, phone FROM $table_name", ARRAY_A);
     return $results;
+}
+
+function url_endpoint_callback($data) {
+    $id = $_GET['id'];
+    $sanitized_id = filter_var($id, FILTER_VALIDATE_INT);
+
+    if ($sanitized_id === false) {
+        return;
+    } else {
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'stories';
+        $query = $wpdb->prepare("SELECT url FROM $table_name WHERE id = %d", $sanitized_id);
+        $results = $wpdb->get_results($query, ARRAY_A);
+        return $results;
+    }
 }
